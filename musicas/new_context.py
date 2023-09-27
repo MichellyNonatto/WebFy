d1a5b3b0fddf38e18157fae73ba499e92f9469b9
Michelly <smartnovo01@gmail.com>
2023-09-26 23:17:08 -0300

    filter

--- musicas/new_context.py
+++ musicas/new_context.py
@@ -1,9 +1,9 @@
 from .models import Musica
 
 def recentes(request):
-    lista_artistas = Musica.objects.all().order_by('-dataCriacao')[0:5]
+    lista_artistas = Musica.objects.all().order_by('-dataCriacao')[0:10]
     return {'recentes': lista_artistas}
 
 def visualizados(request):
-    lista_artistas = Musica.objects.all().order_by('visualizacoes')[0:5]
+    lista_artistas = Musica.objects.all().order_by('visualizacoes')[0:10]
     return {'visualizados': lista_artistas}
