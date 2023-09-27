5188510c7a0979d8678b24046076396e516069aa
Michelly <smartnovo01@gmail.com>
2023-09-26 23:21:43 -0300

    filter

---
+++ staticfiles/js/script.js
@@ -0,0 +1,34 @@
+const btn = document.querySelectorAll('[data-filter]');
+const btnArray = Array.from(btn);
+
+const container = document.querySelectorAll('[data-container]');
+const containerArray = Array.from(container)
+
+console.log(btn)
+const Filtrate = (evento) => {
+    evento.preventDefault();
+
+    const btn_click = evento.target;
+    console.log(btn_click)
+    const indexDoBotaoClicado = btnArray.indexOf(btn_click);
+
+    btnArray.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    btn_click.classList.add("active");
+
+    containerArray.forEach((container, index) => {
+        if (index === indexDoBotaoClicado) {
+            container.classList.add("inline-block");
+            container.classList.remove("hidden");
+        } else {
+            container.classList.remove("inline-block");
+            container.classList.add("hidden");
+        }
+    });
+}
+
+btnArray.forEach((btn) => {
+    btn.addEventListener('click', Filtrate);
+});
