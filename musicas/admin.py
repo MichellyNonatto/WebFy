6baf2bcf40e0c9b09402ccaebc887eeba353cc69
Michelly <smartnovo01@gmail.com>
2023-10-19 21:50:51 -0300

    Classe Usuario

--- musicas/admin.py
+++ musicas/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import Musica, MusicaArtista
+from .models import Musica, MusicaArtista, Usuario
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+campo = list(UserAdmin.fieldsets)
+campo.append(
+    ("Histórico", {'fields':('musica_visto',)})
+)
+
+UserAdmin.fieldsets = tuple(campo)
+
+
 admin.site.register(Musica)
 admin.site.register(MusicaArtista)
-
+admin.site.register(Usuario, UserAdmin)
--- musicas/models.py
+++ musicas/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
 LISTA_CATEGORIAS = {
@@ -32,3 +33,7 @@ class MusicaArtista(models.Model):
 
     def __str__(self):
         return self.artista.titulo + "    -   " + self.titulo
+
+
+class Usuario(AbstractUser):
+    musica_visto = models.ManyToManyField("Musica", related_name="usuario")
